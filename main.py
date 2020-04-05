bec7f4e2fb339fe3ad88f473abcc462abcc7e2f2
mkalino1 <mateusz.kalinowski1998@gmail.com>
2020-04-05 17:28:13 +0200

    Adding Counter

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-# main.py
-
 from fastapi import FastAPI
 
-app = FastAPI()
+from pydantic import BaseModel
 
+app = FastAPI()
+# to see what funny will come
+app.counter = 0
+
+
+class HelloResp(BaseModel):
+    msg: str
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return str(app.counter)
+
+
+@app.get("/hello/{name}", response_model=HelloResp)
 async def read_item(name: str):
-    return f"Hello {name}"
+    return HelloResp(msg=f"Hello {name}")
