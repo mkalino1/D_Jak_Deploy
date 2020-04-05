9f5ab4aafc716a70c92cdead738aad741fc79c28
mkalino1 <mateusz.kalinowski1998@gmail.com>
2020-04-05 15:32:13 +0200

    Adding names

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def read_item(name: str):
+    return f"Hello {name}"
